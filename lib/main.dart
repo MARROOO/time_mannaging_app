4f0e7ab0205693396d7a6f67fe824cea49b32528
Meriam <meriambejaoui12@gmail.com>
2022-04-20 13:25:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title of Application',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("JEENISo project"),
+        
+          actions: <Widget> [
+            IconButton(
+              icon: Icon(Icons.file_upload),
+              onPressed: () => {
+                print("Click on upload button")
+              },
+            ),
+            IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () => {
+                  print("Click on settings button")
+                }
+            ),
+    
+            PopupMenuButton(
+              icon: Icon(Icons.share),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text("Facebook"),
+
+                ),
+                PopupMenuItem(
+                  value: 2,
+                  child: Text("Instagram"),
+                ),
+              ],
+            )
+          ],
+            leading: IconButton(
+              icon:Icon(Icons.menu),
+              onPressed:(){},//icon buttom
+          )
+      ),
+      body: Container(
+       
+      ),
+    );
+  }
+}
